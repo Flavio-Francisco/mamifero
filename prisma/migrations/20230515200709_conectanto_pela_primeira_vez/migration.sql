b552dd54833c19aa21e317eb87d85d3049511c7d
Flavio Francisco <flavyobass151@gmail.com>
2023-05-15 17:11:51 -0300

    conectando ao data base e testando rotas

---
+++ prisma/migrations/20230515200709_conectanto_pela_primeira_vez/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Golfinho" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "species" TEXT NOT NULL,
+    "characteristics" TEXT NOT NULL,
+    "marine" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Golfinho_pkey" PRIMARY KEY ("id")
+);
--- server/server.ts
+++ server/server.ts
@@ -1,22 +1,53 @@
 import Fastify from 'fastify';
-import { Create }from './Create';
+import cors from '@fastify/cors';
+import { z } from 'zod';
+import {PrismaClient}from '@prisma/client';
+
+async  function Server() {
+  const prisma = new PrismaClient();
+  const server = Fastify();
 
 
-const server = Fastify();
 
-
-async function Server() {
-
-
-server.get('/get', async () =>{
+ server.get('/create', async () =>{
   
-  return('rota ok')
+ const Golfinho = await prisma.golfinho.findMany({
+    select:{
+      name:true,
+      species:true,
+      characteristics: true,
+      marine:true,
+    }
+  })
+  return (Golfinho)
 })
+  server.post("/create", async(request, reply)=>{
 
+    const type = z.object({
+      name : z.string(),
+      species: z.string(),
+      characteristics : z.string(),
+      marine : z.boolean(),
+    })
+    const Golfinho = type.parse(request.body);
 
-server.register(Create);
+   if (Golfinho) {
+    
+    prisma.golfinho.create({
+      data:{
+        name: Golfinho.name,
+        species: Golfinho.species,
+        characteristics: Golfinho.characteristics,
+        marine: Golfinho.marine,
+      }
+    })
+    return reply.status(201).send({Golfinho})
+   }else{
+    return reply.send({message:"TÁ TUDO ERRADO!"})
+   }
+  })
 
-    await server.listen({port:3000}, (erro, address)=>{
+    server.listen({port:5432}, (erro, address)=>{
 
     if (erro) {
         server.log.error(erro)
